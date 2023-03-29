796b51f0c0c3ca62ba81c4c1e728cd6e4b970ad9
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-03-29 20:31:07 +0000

    Fix method=POST issue with preventDefault() method

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,17 @@
-localStorage.setItem("username", ""); // Clear the value of the "username" key in local storage
+const form = document.getElementById("user-form");
+localStorage.setItem("username", ""); // Clear the value of the "username" key in local storage.
 
-// Add a click event listener to the "username-save-btn"
+// Add a click event listener to the "username-save-btn element."
 document.getElementById("username-save-btn").addEventListener("click", saveUsername);
 
-/** Save the value of the "username" input to local storage */
+/** Save the value of the "username" input to local storage. */
 function saveUsername() {
 
     localStorage.setItem("username", document.getElementById("username").value);
 }
+
+// Add a "submit" event listener to the form element.
+form.addEventListener("submit", function (event) {
+    
+    event.preventDefault(); // Prevents default submission.
+})
