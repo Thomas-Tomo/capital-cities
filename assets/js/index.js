4abecd2eb58e140ccd07704f9d575c7b37de8be1
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-03-29 20:21:59 +0000

    Add a docstring for saveUsername function

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,8 @@ localStorage.setItem("username", ""); // Clear the value of the "username" key i
 // Add a click event listener to the "username-save-btn"
 document.getElementById("username-save-btn").addEventListener("click", saveUsername);
 
+/** Save the value of the "username" input to local storage */
 function saveUsername() {
-    localStorage.setItem("username", document.getElementById("username").value)
-    console.log(username)
+
+    localStorage.setItem("username", document.getElementById("username").value);
 }
