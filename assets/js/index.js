a3f9eddef588fe2a046f31487bb26d09e0bb01d6
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-03-29 20:18:40 +0000

    Fix issue with username not saving to local storage

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,9 @@
 localStorage.setItem("username", ""); // Clear the value of the "username" key in local storage
-let username = document.getElementById("username").value; // Save username in a variable
-console.log(username)
 
 // Add a click event listener to the "username-save-btn"
 document.getElementById("username-save-btn").addEventListener("click", saveUsername);
 
 function saveUsername() {
-    
+    localStorage.setItem("username", document.getElementById("username").value)
+    console.log(username)
 }
