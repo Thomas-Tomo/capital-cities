ad038d3ab4824bafa75f23d7e6ed16b44911ebce
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-03-29 20:13:18 +0000

    Add username value to a variable and clear the value in a local storage

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,9 @@
+localStorage.setItem("username", ""); // Clear the value of the "username" key in local storage
+let username = document.getElementById("username").value; // Save username in a variable
+console.log(username)
+
 // Add a click event listener to the "username-save-btn"
-document.getElementById("username-save-btn").addEventListener("click", saveUsername)
+document.getElementById("username-save-btn").addEventListener("click", saveUsername);
 
 function saveUsername() {
     
