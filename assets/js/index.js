352814854a7d4030fc39df62c06c32b9d36f2bc6
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-03-29 20:02:36 +0000

    Add event listener to username-save-btn and write an empty function

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,6 @@
-console.log("Hello there")
+// Add a click event listener to the "username-save-btn"
+document.getElementById("username-save-btn").addEventListener("click", saveUsername)
+
+function saveUsername() {
+    
+}
