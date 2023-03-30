ebeb3dc31cfef321d8990ddd6ce9124f181d2a76
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-03-30 16:42:53 +0000

    Set a validation message and add event listener for start game button

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+let validate = document.getElementById("validation").innerText = "";
 const form = document.getElementById("user-form");
 localStorage.setItem("username", ""); // Clear the value of the "username" key in local storage.
 
@@ -17,4 +18,14 @@ form.addEventListener("submit", function (event) {
 
     const input = document.getElementById("username");
     input.value = ""; // Clear the input value.
+
+    validate.innerText = `${localStorage.getItem("username")}, you are all set!`; // Sets a validation message
+    console.log(validate.innerText);
 })
+
+// Add a click event listener to the "start-game-btn" element.
+document.getElementById("start-game-btn").addEventListener("click", initializeQuiz);
+
+function initializeQuiz() {
+
+}
