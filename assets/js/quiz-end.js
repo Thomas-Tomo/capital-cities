f922ae2f8cdccbcd3bf86e1c8af075d1f1f8ce42
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-03-31 15:16:57 +0000

    Add if statement that checks how well did user do,and display appropriate message

--- assets/js/quiz-end.js
+++ assets/js/quiz-end.js
@@ -4,3 +4,13 @@ let user = document.getElementById("user");
 let scoreResult = document.getElementById("score-result");
 
 scoreResult.innerText = `You scored ${mostRecentScore} out of 100 points!`;
+
+if (mostRecentScore >= 90) {
+    user.innerText = `Amazing knowledge, ${username}!`;
+} else if (mostRecentScore >= 70) {
+    user.innerText = `Well done ${username}!`;
+} else if (mostRecentScore >= 50) {
+    user.innerText = `You can do better ${username}!`;
+} else {
+    user.innerText = `That was poor ${username}!`;
+}
