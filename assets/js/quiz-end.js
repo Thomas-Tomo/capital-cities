00471336cb77f49f44308466cc2fa5e4f3dba808
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-03-31 16:04:03 +0000

    Add an if statement that will create an array if it doesnt exist already and retrieve it from local storage highscores

--- assets/js/quiz-end.js
+++ assets/js/quiz-end.js
@@ -3,7 +3,7 @@ let username = localStorage.getItem("username");
 let user = document.getElementById("user");
 let scoreResult = document.getElementById("score-result");
 
-
+// Create an object that stores two properties.
 const quizResult = {username: username, score: score};
 
 // Display quiz score to scoreResult inner text.
@@ -18,4 +18,12 @@ if (mostRecentScore >= 90) {
     user.innerText = `You can do better ${username}!`;
 } else {
     user.innerText = `That was poor ${username}!`;
+}
+
+// Retrieve the existing array of objects from local storage.
+let existingArray = JSON.parse(localStorage.getItem("highscores"));
+
+// If the array doesn't exist yet, create it.
+if (!existingArray) {
+    existingArray = [];
 }
