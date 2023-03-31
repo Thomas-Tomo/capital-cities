b87251d996c3481b5525a95165296afbfa4ed55b
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-03-31 15:22:24 +0000

    Update the code with comments in quiz-end.js

--- assets/js/quiz-end.js
+++ assets/js/quiz-end.js
@@ -3,8 +3,10 @@ let username = localStorage.getItem("username");
 let user = document.getElementById("user");
 let scoreResult = document.getElementById("score-result");
 
+// Display quiz score to scoreResult inner text.
 scoreResult.innerText = `You scored ${mostRecentScore} out of 100 points!`;
 
+// Check how well did the user do and display appropriate message.
 if (mostRecentScore >= 90) {
     user.innerText = `Amazing knowledge, ${username}!`;
 } else if (mostRecentScore >= 70) {
