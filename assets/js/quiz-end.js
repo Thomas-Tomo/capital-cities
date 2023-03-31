f91340af21a013b82680ebe63097fdbe6552936d
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-03-31 14:58:42 +0000

    Add global variables to the quiz-end.js

--- assets/js/quiz-end.js
+++ assets/js/quiz-end.js
@@ -1,1 +1,4 @@
-console.log("Hello")
+let mostRecentScore = localStorage.getItem("mostRecentScore");
+let username = localStorage.getItem("username");
+let user = document.getElementById("user");
+let scoreResult = document.getElementById("score-result");
