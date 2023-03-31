bcc77db5a59805da6e66bc789f6dc49e7e7f79e4
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-03-31 17:27:59 +0000

    Add printHighScore function that creates table name

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,3 +1,21 @@
 const printHighScore = () => {
+
+    // Retrieve highscores from local storage.
+    const highScores = JSON.parse(localStorage.getItem("highscores"));
+
+    // Sort the highscores array by score in descending order.
+    highScores.sort((a, b) => b.score - a.score);
+
+    // Get a reference to the table element.
+    const table = document.getElementById("highscores");
+
+    const nameRow = table.insertRow();
+    const nameCell = nameRow.insertCell();
+    nameCell.colSpan = 2;
+    nameCell.innerText = "High Scores";
+    nameCell.style.fontWeight = "bolder";
+
     
 }
+
+printHighScore()
