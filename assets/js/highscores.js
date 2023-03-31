76aa918433f52cd0b0d568ead6fd812313a96d9d
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-03-31 17:35:14 +0000

    Update printHighScore to create a header row

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -9,13 +9,21 @@ const printHighScore = () => {
     // Get a reference to the table element.
     const table = document.getElementById("highscores");
 
+    // Create table name row
     const nameRow = table.insertRow();
     const nameCell = nameRow.insertCell();
     nameCell.colSpan = 2;
     nameCell.innerText = "High Scores";
     nameCell.style.fontWeight = "bolder";
 
-    
+    // Create header row
+    const headerRow = table.insertRow();
+    const header1 = headerRow.insertCell();
+    const header2 = headerRow.insertCell();
+    header1.innerText = "Username";
+    header2.innerText = "Score";
+    header1.style.fontWeight = "bold";
+    header2.style.fontWeight = "bold";
 }
 
 printHighScore()
