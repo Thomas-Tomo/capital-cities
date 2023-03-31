614849f8bfbfed84f5c6b82f67f7f917a41b30e0
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-03-31 16:18:37 +0000

    Refactor quizResult object to prevent overwriting of username and score

--- assets/js/quiz-end.js
+++ assets/js/quiz-end.js
@@ -3,9 +3,6 @@ let username = localStorage.getItem("username");
 let user = document.getElementById("user");
 let scoreResult = document.getElementById("score-result");
 
-// Create an object that stores two properties.
-const quizResult = {username: username, score: score};
-
 // Display quiz score to scoreResult inner text.
 scoreResult.innerText = `You scored ${mostRecentScore} out of 100 points!`;
 
@@ -19,6 +16,12 @@ if (mostRecentScore >= 90) {
 } else {
     user.innerText = `That was poor ${username}!`;
 }
+
+// Create an object that stores two properties.
+const quizResult = {
+    username: username,
+    score: score
+};
 
 // Retrieve the existing array of objects from local storage.
 let existingArray = JSON.parse(localStorage.getItem("highscores"));
